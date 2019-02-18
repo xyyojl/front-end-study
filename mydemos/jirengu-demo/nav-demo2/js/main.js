0fb399b0fc5b199fa5707669df63161584879ee3
xyyojl <m13631897256@163.com>
2019-02-18 21:17:58 +0800

    添加键盘样式，增加自定义网址功能，优化代码

--- mydemos/jirengu-demo/nav-demo2/js/main.js
+++ mydemos/jirengu-demo/nav-demo2/js/main.js
@@ -1,67 +1,124 @@
+// 1. 初始化数据
+var hashA = init();
+var keys = hashA['keys'];
+var hash = hashA['hash'];
+// 2. 生成键盘
+generateKeyboard(keys,hash);
 
+// 3. 监听用户动作
+listenToUser(hash);
 
-var keys = {
-    '0' : ['q','w','e','r','t','y','u','i','o','p'],
-    '1' : ['a','s','d','f','g','h','j','k','l'],
-    '2' : ['z','x','c','v','b','n','m'],
-    'length' : 3
-}
-
-var hash = {
-    q : 'qq.com',
-    w : 'weibo.com',
-    e : 'ele.me',
-    r : 'renren.com',
-    t : 'tianya.cn',
-    y : 'youtube.com',
-    u : 'uc.cn',
-    i : 'iqiyi.com',
-    o : 'opera.com',
-    p : undefined,
-    a : 'acfun.tv',
-    s : 'sohu.com',
-    z : 'zhihu.com',
-    m : 'mail.163.com'
-}
-// 取出localStorage 里面的zzz对应的hash
-var hashInLocalStorage = JSON.parse(localStorage.getItem('zzz')||null);
-if(hashInLocalStorage){
-    hash = hashInLocalStorage;
-}
-
-
-var index = 0;
-while(index < keys['length']){ // index 0 1 2
-    // 生成三個div
-    var div = document.createElement('div');
-    main.appendChild(div);
-    var index2 = 0;
-    var row = keys[index]; // 第一個數組 第二個數組 第三個數組
-    while (index2 < row['length']) {
-        var kbd = document.createElement('kbd');
-        kbd.textContent = row[index2];
-        div.appendChild(kbd);
-        var button = document.createElement('button');
-        button.textContent = 'E';
-        button.id = row[index2];
-        button.onclick = function(e){
-            key = e['target'].id; // q w e
-            url = prompt('给我一个网址'); // qq.com
-            hash[key] = url; // hash 变更
-            localStorage.setItem('zzz',JSON.stringify(hash));
-        }
-        kbd.appendChild(button);
-        index2 += 1;
+// 下面的是封装的函数
+// 初始化数据相关的函数
+function init() {
+    var keys = {
+        '0' : ['q','w','e','r','t','y','u','i','o','p'],
+        '1' : ['a','s','d','f','g','h','j','k','l'],
+        '2' : ['z','x','c','v','b','n','m'],
+        'length' : 3
     }
-    index += 1;
+    
+    var hash = {
+        q : 'qq.com',
+        w : 'weibo.com',
+        e : 'ele.me',
+        r : 'renren.com',
+        t : 'tianya.cn',
+        y : 'youtube.com',
+        u : 'uc.cn',
+        i : 'iqiyi.com',
+        o : 'opera.com',
+        p : undefined,
+        a : 'acfun.tv',
+        s : 'sohu.com',
+        z : 'zhihu.com',
+        m : 'mail.163.com'
+    }
+    // 取出localStorage 里面的zzz对应的hash
+    var hashInLocalStorage = getFromLocalStorage('zzz');
+    if(hashInLocalStorage){
+        hash = hashInLocalStorage;
+    }
+    return {
+        'keys':keys,
+        'hash':hash
+    }
+}
+function getFromLocalStorage(name){
+    return JSON.parse(localStorage.getItem('zzz')||null);
+}
+// 生成键盘的相关函数
+function generateKeyboard(keys,hash){
+    for (let index = 0; index < keys['length']; index++) {
+        var div = tag('div');
+        div.className = 'row';
+        main.appendChild(div);
+        var row = keys[index];
+        for (let index2 = 0; index2 < row.length; index2++) {
+            var span = createSpan(row[index2]);
+            var button = createButton(row[index2]);
+            var img = createImage(hash[row[index2]]);
+            
+            var kbd = tag('kbd');
+            kbd.className = 'key';
+
+            kbd.appendChild(span);
+            kbd.appendChild(button);
+            kbd.appendChild(img);
+            div.appendChild(kbd);
+        }
+    }
 }
 
-
-document.onkeypress = function(e){
-    var key = e['key'];
-    var website = hash[key];
-    //location.href='http://'+website
-    //location代表地址栏，href代表地址
-    window.open('http://'+website,'_blank');
-    //新窗口打开网页
+function tag(tagName){
+    return document.createElement(tagName);
+}
+function createSpan(textContent) {
+    var span = tag('div');
+    span.className = 'text';
+    span.textContent = textContent;
+    return span;
+} 
+function createButton(id) {
+    var button = tag('button');
+    button.textContent = '编辑';
+    button.id = id;
+    button.onclick = function(e){
+        // 弹出一个框 输入一个网址 改变图片
+        var button2 = e.target;
+        var img2 = button2.nextSibling;
+        var key = e.target.id; // q w e
+        var url = prompt('给我一个网址'); // qq.com
+        hash[key] = url; // 哈希变更
+        img2.src = img2.src='http://'+url+'/favicon.ico';
+        img2.onerror = function(e){
+            e.target.src='//i.loli.net/2017/11/10/5a05afbc5e183.png';
+        }
+        localStorage.setItem('zzz',JSON.stringify(hash));
+    }
+    return button;
+}
+function createImage(domain) {
+    var img = tag('img');
+    if(domain){
+        img.src = 'http://'+domain+'/favicon.ico';
+    }else{
+        img.src = '//i.loli.net/2017/11/10/5a05afbc5e183.png';
+    }
+    img.onerror = function(e){
+        e.target.src='//i.loli.net/2017/11/10/5a05afbc5e183.png';
+    }
+    return img;
+}
+ 
+// 监听用户事件的函数
+function listenToUser(hash) {
+    document.onkeypress = function(e){
+        var key = e['key'];
+        var website = hash[key];
+        //location.href='http://'+website
+        //location代表地址栏，href代表地址
+        window.open('http://'+website,'_blank');
+        //新窗口打开网页
+    }
 }
